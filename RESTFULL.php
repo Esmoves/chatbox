a1e5736c098dd37f651921995f4a76aa2bb948bb
Esmeralda <34519178+Esmoves@users.noreply.github.com>
2018-01-24 16:42:15 +0100

    versie 2
    Externe HTML file moet URL produceren met GET en waarden meegeven
    Bijvoorbeeld op mijn computer http://localhost/websites/restfullPHPChat/RESTFULL.php?mykey=12345&value=hoihoi
    RESTFULL.php leest de URL en geeft de waarde van de mykey en value weer.

--- RESTFULL.php
+++ RESTFULL.php
@@ -1,23 +1,20 @@
 <?php
 /*
 needed: id, mykey, value, minimumid Get all messages with an id higher than or equal to minimumid)
-Get api.php
-Put api.php
-need a url zonder /
+_Get api.php
+_Put api.php
 
 Opdracht: bouw een API die communiceert tussen een HTML pagina met formulieren en je MySql database
 
 */
 // GET stuur mykey mee en de id die je wilt laten
 
-
-
-
+/*
 $id = "1";
 $minimumid = "0";
 $value = "My first message";
 $mykey = "12345";
-$URL = "http://localhost/websites/restfullPHPChat/index.php";
+*/
 
 // functie stuur bericht naar server via PUT en bewaar de id
 
@@ -34,18 +31,22 @@ $URL = "http://localhost/websites/restfullPHPChat/index.php";
 
 
 
+// id moet erbij en werken met PUT en GET
+// filegetdocument? // _PATCH ? JSON parse
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["room"])) {
+
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+  if (empty($_GET["mykey"])) {
     $roomErr = "Chatroom is required";
   } else {
-    $room = test_input($_POST["room"]);
+    $room = test_input($_GET["mykey"]);
   }
   
-  if (empty($_POST["value"])) {
-    $valueErr = "A message is required";
+  if (empty($_GET["value"])) {
+    $valueErr = "ERROR. Message is required";
   } else {
-    $value = test_input($_POST["value"]);
+    $value = test_input($_GET["value"]);
   }
    // output
 	echo "Welcome in the chatroom" . $room . "<br> Message was:" . $value;
@@ -59,48 +60,30 @@ function test_input($data) {
 }
 
 
+/* ALTERNATIEVE METHODE VAN TUTORIAL 
+    WERKT NOG NIET OPTIMAAL  
+
+$verb = $_SERVER['REQUEST_METHOD']; 
+
+if ($verb == 'GET'){
+  if (isset($_GET['mykey'])){
+    $file_content = file_get_contents($_GET['mykey']);
+
+    // url heeft een GET en mykey, dus kun je alle berichten uit de mykey room halen
+
+    echo $file_content;
+    } else {
+      die("ERROR");
+      }
+   } 
+   elseif ($verb == 'PUT'){
+    if (isset($_PUT['id']) and isset($_PUT['mykey']) and isset($_PUT['value'])){
+      file_put_contents($_PUT['id'], $_PUT['mykey'], $_PUT['value']);
+    } 
+  }
+*/
+
+
+
+
 	?>
-
-<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN'>
-
-<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='nl' lang='nl'>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
-    <meta name="description" content="Chatt app by Esmeralda Tijhoff" />  
-    <meta name="keywords" content="" />
-    <meta name="author" content="A.E.Tijhoff" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
-
- 
-
-   <title>Chatt App</title>
-</head>
-<body>
-
-   <div id="container">
-      <div class='titel'>
-
-        <h1 class='titel'>
-            Chatt App
-        </h1>
-    </div>
-
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-        Chatroom: <input type="text" name="room" value=""><br>
-        Message: <input type="text" name="bericht"><br>
-        <input type="submit" name="submit" value="Submit">
-        </form>
-
-      </div>
-
-<?php
-
-
-
-
-?>
-
-
-</body>
-</html>
