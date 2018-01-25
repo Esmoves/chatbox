29e4125fe98e3ed9bc890d6100d1ad72f08b23d3
Esmeralda <34519178+Esmoves@users.noreply.github.com>
2018-01-25 16:01:17 +0100

    class message || connect to json || GET messages
    Created a class with id, mykey and value.
    Request_Method GET --> open database.json and get messages 
    Start PUT request
    
    test with api.php?mykey=12345

--- restfull_wk2/api.php
+++ restfull_wk2/api.php
@@ -1,89 +1,84 @@
 <?php
 /*
-needed: id, mykey, value, minimumid Get all messages with an id higher than or equal to minimumid)
+Uit REST document: id, mykey, value, minimumid Get all messages with an id higher than or equal to minimumid)
 _Get api.php
 _Put api.php
 
-Opdracht: bouw een API die communiceert tussen een HTML pagina met formulieren en je MySql database
+Opdracht: bouw een API die communiceert tussen een HTML pagina met formulieren --> javascript verwerkings file --> api.php --> MySql database
 
 */
+
 // GET stuur mykey mee en de id die je wilt laten
-
-/*
-$id = "1";
-$minimumid = "0";
-$value = "My first message";
-$mykey = "12345";
-*/
-
 // functie stuur bericht naar server via PUT en bewaar de id
-
-
 // functie haal bericht op met de id 
-
 // functie haal alle berichten op met id => minimumid
-
 // loop door alle berichten
-
 // haal bericht één voor één op in de loop 
-
 // geef verschillende class aan zelf verzonden en aan ontvangen messages
-
-
 
 // id moet erbij en werken met PUT en GET
 // filegetdocument? // _PATCH ? JSON parse
 
 
+// class maken met message bevat id, mykey en value
+// dan kun je steeds nieuwe messages aanmaken als een Json dus $message(id, mykey, value)
+
+class message {
+    private $id;
+    private $mykey;
+    private $value;
+
+        public function __construct($p_id, $p_mykey, $p_value) {
+              $this->id = $p_id;
+              $this->mykey = $p_mykey;
+              $this->value = $p_value;
+            }
+}
+
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-  if (empty($_GET["mykey"])) {
-    $roomErr = "Chatroom is required";
-  } else {
-    $room = test_input($_GET["mykey"]);
-  }
-  
-  if (empty($_GET["value"])) {
-    $valueErr = "ERROR. Message is required";
-  } else {
-    $value = test_input($_GET["value"]);
-  }
-   // output
-	echo "Welcome in the chatroom" . $room . "<br> Message was:" . $value;
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-
-
-/* ALTERNATIEVE METHODE VAN TUTORIAL 
-    WERKT NOG NIET OPTIMAAL  
-
-$verb = $_SERVER['REQUEST_METHOD']; 
-
-if ($verb == 'GET'){
-  if (isset($_GET['mykey'])){
-    $file_content = file_get_contents($_GET['mykey']);
-
-    // url heeft een GET en mykey, dus kun je alle berichten uit de mykey room halen
-
-    echo $file_content;
-    } else {
-      die("ERROR");
-      }
-   } 
-   elseif ($verb == 'PUT'){
-    if (isset($_PUT['id']) and isset($_PUT['mykey']) and isset($_PUT['value'])){
-      file_put_contents($_PUT['id'], $_PUT['mykey'], $_PUT['value']);
+    if (empty($_GET["mykey"])) {
+      $roomErr = "Chatroom is required <br />";
+      echo $roomErr;
     } 
-  }
-*/
+    else {
+      $myfile = fopen("database.json", "r") or die("Cannot open file: ".$my_file);
+      $data = json_decode(fread($myfile,filesize("database.json")));
+      fclose($myfile);
 
+      $minimumID = 1;
+      $requestedMessages = array();
+          
+      if(isset($_GET["minimumid"])){
+          $minimumID = $_GET["minimumid"];
+      }
+      
+      $i;
+      for($i = 0; $i < count($data); $i++){
+          if($data[$i]->id>=$minimumID && $data[$i]->mykey==$_GET["mykey"]){
+          array_push($requestedMessages, $data[$i]);
+          }   
+             
+      }      echo json_encode($requestedMessages);
+ }    
+}
+  
 
+if ($_SERVER["REQUEST_METHOD"] == "PUT") {
+    if (empty($_GET["mykey"])) {
+      $roomErr = "Chatroom is required";
+    } 
+    else if (empty($_GET["value"])) {
+      $valueErr = "ERROR. Message is required";
+    } 
+    else {
+      $myfile = fopen("database.json", "w") or die("Cannot open file: ".$my_file);
+      $data = json_decode(fread($myfile,filesize("database.json")));
+      fwrite($myfile, $data);
+      }
+ }
+
+  
 
 
 	?>
